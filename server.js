48fb5dcc1a04f67f0d76a9bc1ad8ad9b4439d400
Ashwinmano <ashwin.m@sceince.christuniversity.in>
2021-05-06 01:04:35 +0530

    deployment

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const usac = require("./Routes/usac");
 const webinars = require("./Routes/webinars");
 const csaincoming = require("./Routes/csaincomingstud");
 const concat = require('./Routes/concat');
+const path = require('path');
 require("dotenv/config");
 
 app.use(express.json());
@@ -51,6 +52,14 @@ mongoose.connect(
     console.log("Connected to DB");
   }
 );
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  } )
+}
 
 port = process.env.PORT || 8001;
 app.listen(port, () => {
